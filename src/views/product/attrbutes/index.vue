1e1bb778c9a2f92d6b2906a487e833b012db094f
Dennis <740125aa@gmail.com>
2023-11-16 20:51:32 +0800

    修正返回按鈕時數據的顯示問題

--- src/views/product/attrbutes/index.vue
+++ src/views/product/attrbutes/index.vue
@@ -1,23 +1,43 @@
 <template>
   <div>
-    <el-card style="margin: 20px 0;">
+    <el-card style="margin: 20px 0">
       <CategorySelect @getCategoryId="getCategoryId" />
     </el-card>
     <el-card>
       <div v-show="isShowTable">
-        <el-button type="primary" icon="el-icon-plus" :disabled="!category3Id" @click="isShowTable=false">添加屬性</el-button>
+        <el-button
+          type="primary"
+          icon="el-icon-plus"
+          :disabled="!category3Id"
+          @click="addAttr"
+        >添加屬性</el-button>
         <!-- 表格 -->
-        <el-table style="width: 100%;" border :data="attrList">
-          <el-table-column type="index" label="序號" width="80" align="center" />
+        <el-table style="width: 100%" border :data="attrList">
+          <el-table-column
+            type="index"
+            label="序號"
+            width="80"
+            align="center"
+          />
           <el-table-column prop="attrName" label="屬性名稱" width="150" />
           <el-table-column prop="" label="屬性列表">
-            <template slot-scope="{row,$index}">
-              <el-tag v-for="(attrValue, index) in row.attrValueList" :key="attrValue.id" type="success" style="margin: 0px 20px;">{{ attrValue.valueName }}</el-tag>
+            <template slot-scope="{ row, $index }">
+              <el-tag
+                v-for="(attrValue, index) in row.attrValueList"
+                :key="attrValue.id"
+                type="success"
+                style="margin: 0px 20px"
+              >{{ attrValue.valueName }}</el-tag>
             </template>
           </el-table-column>
           <el-table-column prop="" label="操作" width="150">
-            <template slot-scope="{row,$index}">
-              <el-button type="warning" icon="el-icon-edit" size="mini" @click="isShowTable=false" />
+            <template slot-scope="{ row, $index }">
+              <el-button
+                type="warning"
+                icon="el-icon-edit"
+                size="mini"
+                @click="isShowTable = false"
+              />
               <el-button type="danger" icon="el-icon-delete" size="mini" />
             </template>
           </el-table-column>
@@ -25,28 +45,55 @@
       </div>
       <!-- 添加屬性和修改屬性的結構 -->
       <div v-show="!isShowTable">
-        <el-form ref="form" :rules="rules" label-width="80px" :inline="true" :model="attrInfo">
+        <el-form
+          ref="form"
+          :rules="rules"
+          label-width="80px"
+          :inline="true"
+          :model="attrInfo"
+        >
           <el-form-item label="屬性名">
             <el-input v-model="attrInfo.attrName" placeholder="請輸入屬性名" />
           </el-form-item>
         </el-form>
-        <el-button type="primary" icon="el-icon-plus" :disabled="!attrInfo.attrName" @click="addAttrValue">添加屬性值</el-button>
-        <el-button @click="isShowTable=true">取消</el-button>
-        <el-table border style="width: 100%;margin: 20px 10px;" :data="attrInfo.attrValueList">
-          <el-table-column type="index" label="序號" width="80" align="center" />
+        <el-button
+          type="primary"
+          icon="el-icon-plus"
+          :disabled="!attrInfo.attrName"
+          @click="addAttrValue"
+        >添加屬性值</el-button>
+        <el-button @click="isShowTable = true">取消</el-button>
+        <el-table
+          border
+          style="width: 100%; margin: 20px 10px"
+          :data="attrInfo.attrValueList"
+        >
+          <el-table-column
+            type="index"
+            label="序號"
+            width="80"
+            align="center"
+          />
           <el-table-column prop="attrName" label="屬性值名稱" width="width">
-            <template slot-scope="{row,$index}">
-              <el-input v-model="row.valueName" placeholder="請輸入屬性值名稱" size="mini" />
+            <template slot-scope="{ row, $index }">
+              <el-input
+                v-model="row.valueName"
+                placeholder="請輸入屬性值名稱"
+                size="mini"
+              />
             </template>
           </el-table-column>
           <el-table-column prop="" label="操作" width="width">
-            <template slot-scope="{row,$index}">
+            <template slot-scope="{ row, $index }">
               <el-button type="danger" icon="el-icon-delete" size="mini" />
             </template>
           </el-table-column>
         </el-table>
-        <el-button type="primary" :disabled="!attrInfo.attrName">保存</el-button>
-        <el-button @click="isShowTable=true">取消</el-button>
+        <el-button
+          type="primary"
+          :disabled="!attrInfo.attrName"
+        >保存</el-button>
+        <el-button @click="isShowTable = true">取消</el-button>
       </div>
     </el-card>
   </div>
@@ -63,14 +110,12 @@ export default {
       attrList: [],
       isShowTable: true,
       // 收集新增屬性
-      attrInfo:
-        {
-          'attrName': '',
-          'attrValueList': [],
-          'categoryId': 0,
-          'categoryLevel': 3
-        }
-
+      attrInfo: {
+        attrName: '',
+        attrValueList: [],
+        categoryId: 0,
+        categoryLevel: 3
+      }
     }
   },
   methods: {
@@ -90,23 +135,33 @@ export default {
     },
     async getAttrList() {
       const { category1Id, category2Id, category3Id } = this
-      const result = await this.$API.attr.reqAttrList(category1Id, category2Id, category3Id)
+      const result = await this.$API.attr.reqAttrList(
+        category1Id,
+        category2Id,
+        category3Id
+      )
       if (result.code === 200) {
         this.attrList = result.data
       }
     },
     addAttrValue() {
-      this.attrInfo.attrValueList.push(
-        {
-          'attrId': undefined,
-          'valueName': ''
-        }
-      )
+      this.attrInfo.attrValueList.push({
+        attrId: undefined,
+        valueName: ''
+      })
+    },
+    addAttr() {
+      this.isShowTable = false
+      this.attrInfo = {
+        attrName: '',
+        attrValueList: [],
+        categoryId: this.category3Id,
+        categoryLevel: 3
+      }
     }
   }
 }
 </script>
 
-<style>
-</style>
+<style></style>
 
