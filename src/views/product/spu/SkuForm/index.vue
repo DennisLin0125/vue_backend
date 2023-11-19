59542cb7524110d37935546971cf2118654b6952
Dennis <740125aa@gmail.com>
2023-11-19 19:56:38 +0800

    展示SKU與收集SKU的資料完成

--- src/views/product/spu/SkuForm/index.vue
+++ src/views/product/spu/SkuForm/index.vue
@@ -1,27 +1,28 @@
 <template>
   <div>
     <el-form ref="form" label-width="80px">
-      <el-form-item label="SPU名稱">海綿寶寶</el-form-item>
+      <el-form-item label="SPU名稱">{{ spu.spuName }}</el-form-item>
       <el-form-item label="SKU名稱">
-        <el-input placeholder="SKU名稱" />
+        <el-input v-model="skuInfo.skuName" placeholder="SKU名稱" />
       </el-form-item>
       <el-form-item label="價格(元)">
-        <el-input placeholder="價格(元)" />
+        <el-input v-model="skuInfo.price" placeholder="價格(元)" type="number" />
       </el-form-item>
       <el-form-item label="重量(kg)">
-        <el-input placeholder="重量(kg)" />
+        <el-input v-model="skuInfo.weight" placeholder="重量(kg)" />
       </el-form-item>
       <el-form-item label="規格描述">
-        <el-input type="textarea" placeholder="規格描述" rows="4" />
+        <el-input v-model="skuInfo.skuDesc" type="textarea" placeholder="規格描述" rows="4" />
       </el-form-item>
       <el-form-item label="平台屬性">
         <el-form ref="form" label-width="80px" :inline="true">
-          <el-form-item label="螢幕尺寸">
-            <el-select placeholder="請選擇" value="">
+          <el-form-item v-for="(attr) in attrInfoList" :key="attr.id" :label="attr.attrName">
+            <el-select v-model="attr.attrIdAndValueId" placeholder="請選擇">
               <el-option
-                key="item.value"
-                label="item.label"
-                value="item.value"
+                v-for="(attrValue) in attr.attrValueList"
+                :key="attrValue.id"
+                :label="attrValue.valueName"
+                :value="`${attr.id}:${attrValue.id}`"
               />
             </el-select>
           </el-form-item>
@@ -30,12 +31,12 @@
 
       <el-form-item label="銷售屬性">
         <el-form ref="form" label-width="80px" :inline="true">
-          <el-form-item label="顏色">
-            <el-select placeholder="請選擇" value="">
+          <el-form-item v-for="saleAttr in spuSaleAttrList" :key="saleAttr.id" :label="saleAttr.saleAttrName">
+            <el-select placeholder="請選擇" v-model="saleAttr.attrIdAndValueId">
               <el-option
-                key="item.value"
-                label="item.label"
-                value="item.value"
+                v-for="(saleAttrValue) in saleAttr.spuSaleAttrValueList" :key="saleAttrValue.id"
+                :label="saleAttrValue.saleAttrValueName"
+                :value="`${saleAttr.id}:${saleAttrValue.id}`"
               />
             </el-select>
           </el-form-item>
@@ -70,12 +71,38 @@ export default {
     return {
       spuImageLIst: [],
       spuSaleAttrList: [],
-      attrInfoList: []
+      attrInfoList: [],
+      skuInfo: {
+        // 第一類收集的數據：父組件給的數據
+        category3Id: 0,
+        spuId: 0,
+        tmId: 0,
+        // 第二類：需要透過資料雙向綁定v-model收集
+        skuName: '',
+        price: 0,
+        weight: '',
+        skuDesc: '',
+        // 第三類：需要自己書寫程式碼
+        // 預設圖片
+        skuDefaultImg: '',
+        // 收集圖片的字段
+        skuImageList: [],
+        // 平台屬性
+        skuAttrValueList: [],
+        // 銷售屬性
+        skuSaleAttrValueList: []
+      },
+      spu: {}
     }
   },
   methods: {
     // 獲取數據
     async getData(category1Id, category2Id, spu) {
+      // 收集父組件給的訊息
+      this.skuInfo.category3Id = spu.category3Id
+      this.skuInfo.spuId = spu.id
+      this.skuInfo.tmId = spu.tmId
+      this.spu = spu
       // 獲取圖片訊息
       const result = await this.$API.spu.reqSpuImageLIst(spu.id)
       if (result.code === 200) {
