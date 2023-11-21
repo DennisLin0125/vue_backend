8c8002f49cf7d3727991441fb2119065c766dea0
Dennis <740125aa@gmail.com>
2023-11-21 20:34:50 +0800

    SKU上架與下架操作

--- src/views/product/sku/index.vue
+++ src/views/product/sku/index.vue
@@ -1,28 +1,34 @@
 <template>
   <div>
-    <el-table border style="width: 100%;" :data="records">
+    <el-table border style="width: 100%" :data="records">
       <el-table-column type="index" label="序號" width="85" align="center" />
       <el-table-column prop="skuName" label="名稱" width="width" />
       <el-table-column prop="skuDesc" label="描述" width="width" />
       <el-table-column prop="col.id" label="預設圖片" width="110">
-        <template slot-scope="{row,$index}">
+        <template slot-scope="{ row, $index }">
           <img :src="row.skuDefaultImg" height="100px" width="100px">
         </template>
       </el-table-column>
       <el-table-column prop="weight" label="重量(kg)" width="80" />
       <el-table-column prop="price" label="價格(元)" width="80" />
       <el-table-column prop="col.id" label="操作" width="width">
-        <template slot-scope="{row,$index}">
-          <el-button type="success" size="mini" icon="el-icon-bottom" />
-          <el-button type="success" size="mini" icon="el-icon-top" />
-          <el-button type="primary" size="mini" icon="el-icon-edit" />
+        <template slot-scope="{ row, $index }">
+          <el-button
+            v-if="row.isSale == 0"
+            type="success"
+            size="mini"
+            icon="el-icon-bottom"
+            @click="sale(row)"
+          />
+          <el-button v-else type="success" size="mini" icon="el-icon-top" @click="cancel(row)" />
+          <el-button type="primary" size="mini" icon="el-icon-edit" @click="edit" />
           <el-button type="info" size="mini" icon="el-icon-time" />
           <el-button type="danger" size="mini" icon="el-icon-delete" />
         </template>
       </el-table-column>
     </el-table>
     <el-pagination
-      style="text-align: center;"
+      style="text-align: center"
       :current-page="page"
       :page-sizes="[3, 5, 10]"
       :page-size="limit"
@@ -31,7 +37,6 @@
       @current-change="getSkuList"
       @size-change="sizeChange"
     />
-
   </div>
 </template>
 
@@ -62,9 +67,46 @@ export default {
     sizeChange(limit) {
       this.limit = limit
       this.getSkuList()
+    },
+    async sale(sku) {
+      const result = await this.$API.sku.reqSale(sku.id)
+      if (result.code === 200) {
+        // eslint-disable-next-line require-atomic-updates
+        sku.isSale = 1
+        this.$message({
+          type: 'success',
+          message: '上架成功'
+        })
+      } else {
+        this.$message({
+          type: 'error',
+          message: '上架失敗'
+        })
+      }
+    },
+    async cancel(sku) {
+      const result = await this.$API.sku.reqCancel(sku.id)
+      if (result.code === 200) {
+        // eslint-disable-next-line require-atomic-updates
+        sku.isSale = 0
+        this.$message({
+          type: 'success',
+          message: '下架成功'
+        })
+      } else {
+        this.$message({
+          type: 'error',
+          message: '下架失敗'
+        })
+      }
+    },
+    edit() {
+      this.$message({
+        type: 'info',
+        message: '正在開發中'
+      })
     }
   }
-
 }
 </script>
 
