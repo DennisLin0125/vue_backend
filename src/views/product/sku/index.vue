0edd5c465555edcfbd96163d8b002f9db92a9bc0
Dennis <740125aa@gmail.com>
2023-11-21 21:30:51 +0800

    SKU詳情查看完成

--- src/views/product/sku/index.vue
+++ src/views/product/sku/index.vue
@@ -20,9 +20,25 @@
             icon="el-icon-bottom"
             @click="sale(row)"
           />
-          <el-button v-else type="success" size="mini" icon="el-icon-top" @click="cancel(row)" />
-          <el-button type="primary" size="mini" icon="el-icon-edit" @click="edit" />
-          <el-button type="info" size="mini" icon="el-icon-time" />
+          <el-button
+            v-else
+            type="success"
+            size="mini"
+            icon="el-icon-top"
+            @click="cancel(row)"
+          />
+          <el-button
+            type="primary"
+            size="mini"
+            icon="el-icon-edit"
+            @click="edit"
+          />
+          <el-button
+            type="info"
+            size="mini"
+            icon="el-icon-time"
+            @click="getSkuInfo(row)"
+          />
           <el-button type="danger" size="mini" icon="el-icon-delete" />
         </template>
       </el-table-column>
@@ -37,6 +53,44 @@
       @current-change="getSkuList"
       @size-change="sizeChange"
     />
+    <!-- 抽屜 -->
+    <el-drawer :visible.sync="show" :show-close="false" size="50%">
+      <el-row>
+        <el-col :span="5">名稱</el-col>
+        <el-col :span="16">{{ skuInfo.skuName }}</el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="5">描述</el-col>
+        <el-col :span="16">{{ skuInfo.skuDesc }}</el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="5">價格</el-col>
+        <el-col :span="16">{{ skuInfo.price }}</el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="5">平台屬性</el-col>
+        <el-col :span="16">
+          <template>
+            <el-tag
+              v-for="(item, index) in skuInfo.skuAttrValueList"
+              :key="item.id"
+              type="success"
+              style="margin-right: 10px;"
+            >{{ item.attrId }}-{{ item.valueId }}</el-tag>
+          </template>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="5">商品圖片</el-col>
+        <el-col :span="16">
+          <el-carousel height="150px">
+            <el-carousel-item v-for="item in skuInfo.skuImageList" :key="item.id">
+              <img :src="item.imgUrl" >
+            </el-carousel-item>
+          </el-carousel>
+        </el-col>
+      </el-row>
+    </el-drawer>
   </div>
 </template>
 
@@ -48,7 +102,9 @@ export default {
       page: 1,
       limit: 3,
       records: [],
-      total: 0
+      total: 0,
+      skuInfo: {},
+      show: false
     }
   },
   mounted() {
@@ -105,11 +161,46 @@ export default {
         type: 'info',
         message: '正在開發中'
       })
+    },
+    async getSkuInfo(sku) {
+      this.show = true
+      const result = await this.$API.sku.reqSkuById(sku.id)
+      if (result.code === 200) {
+        this.skuInfo = result.data
+      }
     }
   }
 }
 </script>
 
 <style>
+.el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 150px;
+    margin: 0;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+     background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n+1) {
+     background-color: #d3dce6;
+  }
+  .el-carousel__button{
+    width: 10px;
+    height: 10px;
+    background: red;
+    border-radius: 50%;
+  }
+.el-row .el-col-5 {
+  font-size: 18px;
+  text-align: right;
+}
+.el-col {
+  margin: 10px 10px;
+}
 </style>
 
